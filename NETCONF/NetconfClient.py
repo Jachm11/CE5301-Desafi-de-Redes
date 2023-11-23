4d5c5722b1de1d58eff9c0cfdff48db88764c5e3
Jachm <joalchama@gmail.com>
2023-11-23 01:35:48 -0600

    Add netconf class ready

--- NETCONF/NetconfClient.py
+++ NETCONF/NetconfClient.py
@@ -1,5 +1,5 @@
 from ncclient import manager
-from xml.etree import ElementTree as ET
+import xmltodict
 
 class NetconfClient:
     def __init__(self, host, port, username, password):
@@ -9,6 +9,8 @@ class NetconfClient:
         self.password = password
         self.device = None
 
+    # ------------Connection methods--------------
+
     def connect(self) -> None:
         try:
             # Establish a NETCONF connection
@@ -17,9 +19,10 @@ class NetconfClient:
                 port=self.port,
                 username=self.username,
                 password=self.password,
-                device_params={'name': 'nexus'},
-                timeout=10,
-                hostkey_verify=False
+                device_params={'name': 'default'},
+                timeout=30,
+                hostkey_verify=False,
+                allow_agent=False
             )
             print("Connected to the device.")
         except Exception as e:
@@ -30,6 +33,8 @@ class NetconfClient:
             self.device.close_session()
             print("Disconnected from the device.")
 
+    # ------------<get-config>--------------
+
     def get_config(self, source='running', filter_str=None, save_to_file=False, file_name='config.xml') -> str:
         try:
             # Issue a NETCONF <get-config> operation
@@ -39,32 +44,144 @@ class NetconfClient:
 
             # Save to file if the flag is set
             if save_to_file:
-                self.save_to_file(file_name, config_data)
+                self._save_str_to_file(config_data,file_name)
 
             return config_data
 
         except Exception as e:
             print(f"Failed to retrieve configuration. Error: {str(e)}")
 
-    def save_to_file(self, file_name, data):
-        try:
-            with open(file_name, 'w') as file:
-                file.write(data)
-            print(f"Configuration saved to {file_name}")
-
-        except Exception as e:
-            print(f"Failed to save configuration to {file_name}. Error: {str(e)}")
-
-    def get_capabilities(self) -> dict:
+    def get_capabilities(self, save_to_file=False, file_name='capabilities.txt') -> dict:
         if self.device:
             try:
-                return self.device.server_capabilities
+                capabilities = self.device.server_capabilities
+
+                # Save to file if the flag is set
+                if save_to_file:
+                    self. _save_dict_to_file(capabilities, file_name)
+
+                return capabilities
+            
             except Exception as e:
                 print(f"Failed to retrieve capabilities. Error: {str(e)}")
         else:
             print(f"Error: Device not connected")
 
-    # UNTESTED (pero deberia funcionar)
+    def get_schema(self, schema, save_to_file=False, file_name="schema.txt"):
+        try:
+
+            schema_str = self.device.get_schema(schema)
+
+            # Save to file if the flag is set
+            if save_to_file:
+                self._save_str_to_file(str(schema_str), file_name)
+
+            return schema
+
+        except Exception as e:
+            print(f"Failed to retrieve configuration. Error: {str(e)}")
+
+    def get_serial(self):
+        """
+        Get system's serial number for a cisco-nx-os-device
+        """
+        serial_number = """
+        <System xmlns="http://cisco.com/ns/yang/cisco-nx-os-device">
+            <serial/>
+        </System>
+        """
+        netconf_response = self.device.get(('subtree', serial_number))
+
+        # Parse the XML and print the data
+        xml_data = netconf_response.data_ele
+        serial =  xml_data.find(".//{http://cisco.com/ns/yang/cisco-nx-os-device}serial").text
+        
+        return serial
+
+    def get_loopbacks(self) -> str:
+        """
+        Get the loopbacks of a cisco-nx-os-device
+        """
+
+        loopback_filter = """
+        <filter>
+            <System xmlns="http://cisco.com/ns/yang/cisco-nx-os-device">
+                <intf-items>
+                    <lb-items></lb-items>
+                </intf-items>
+            </System>
+        </filter>
+        """
+
+        ipv4_filter_template = """
+        <filter>
+            <System xmlns="http://cisco.com/ns/yang/cisco-nx-os-device">
+                <ipv4-items>
+                    <inst-items>
+                        <dom-items>
+                            <Dom-list>
+                                <name>default</name>
+                                <if-items>
+                                    <If-list>
+                                        <id>{0}</id>
+                                    </If-list>
+                                </if-items>
+                            </Dom-list>
+                        </dom-items>
+                    </inst-items>
+                </ipv4-items>
+            </System>
+        </filter>
+        """
+
+        netconf_response = self.device.get(filter=loopback_filter)
+        netconf_data = xmltodict.parse(netconf_response.xml)["rpc-reply"]["data"]
+
+        try:
+            loopbacks = netconf_data["System"]["intf-items"]["lb-items"]["LbRtdIf-list"]
+        except KeyError:
+            print("There are no loopback interfaces currently configured.")
+
+        if not type(loopbacks) is list:
+            # Create a list with single object
+            loopbacks = [loopbacks]
+        
+        response = ""
+
+        for loopback in loopbacks:
+
+            print(loopback.keys())
+            response += "Interface Id: {}\n".format(loopback["id"])
+            response += "Admin State: {}\n".format(loopback["adminSt"])
+            response += "Oper State: {}\n".format(loopback["lbrtdif-items"]["operSt"])
+            try:
+                response += "Description: {}\n".format(loopback["descr"])
+            except:
+                response += "Description:\n"
+
+            # Retrieve IPv4 Address for Loopback
+            ipv4_filter = ipv4_filter_template.format(loopback["id"])
+            ipv4_response = self.device.get(filter=ipv4_filter)
+            ipv4_data = xmltodict.parse(ipv4_response.xml)["rpc-reply"]["data"]
+
+            # See if IPv4 Addresses exist
+            try:
+                ipv4_addresses = ipv4_data["System"]["ipv4-items"]["inst-items"]["dom-items"]["Dom-list"]["if-items"]["If-list"]["addr-items"]["Addr-list"]
+                # Ensure list of addresses
+                if not type(ipv4_addresses) is list:
+                    ipv4_addresses = [ipv4_addresses]
+
+                primary_ipv4_address = ipv4_addresses[0]
+
+                # Print IPv4 Address Info
+                response += "    IPv4 Address: {}\n\n".format(primary_ipv4_address["addr"])
+            except KeyError:
+                response += "     There are no IPv4 addresses configured.\n\n"
+
+        return response
+
+    # ------------<edit-config>--------------
+
     def edit_config(self, config_data, target='running') -> None:
         try:
             # Issue a NETCONF <edit-config> operation
@@ -75,7 +192,62 @@ class NetconfClient:
         except Exception as e:
             print(f"Failed to edit configuration. Error: {str(e)}")
 
-    # UNTESTED (no creo que funcione)
+    def add_loopback(self, loopback_number:int, loopback_ip, description) -> str:
+        """
+        Add a loopback on a cisco-nx-os-device
+        """
+        LOOPBACK_IP = { 'loopback': 'lo{}'.format(loopback_number),
+                        'ip': loopback_ip,
+                        'name': 'Loopback{}'.format(loopback_number),
+                        'description': description
+                    }
+
+        add_ip_interface = """
+        <config>
+            <System xmlns="http://cisco.com/ns/yang/cisco-nx-os-device">
+                <intf-items>
+                    <lb-items>
+                        <LbRtdIf-list>
+                            <id>{0}</id>
+                            <adminSt>up</adminSt>
+                            <descr>{2}</descr>
+                        </LbRtdIf-list>
+                    </lb-items>
+                </intf-items>
+                <ipv4-items>
+                    <inst-items>
+                        <dom-items>
+                            <Dom-list>
+                                <name>default</name>
+                                <if-items>
+                                    <If-list>
+                                        <id>{0}</id>
+                                        <addr-items>
+                                            <Addr-list>
+                                                <addr>{1}</addr>
+                                            </Addr-list>
+                                        </addr-items>
+                                    </If-list>
+                                </if-items>
+                            </Dom-list>
+                        </dom-items>
+                    </inst-items>
+                </ipv4-items>
+            </System>
+        </config>
+        """
+        new_ip = add_ip_interface.format(
+                LOOPBACK_IP['loopback'],
+                LOOPBACK_IP['ip'],
+                LOOPBACK_IP['description']
+            )
+        
+        netconf_response = self.device.edit_config(target='running', config=new_ip)
+
+        return netconf_response
+
+    # ------------<copy-config>--------------
+
     def copy_config(self, source, target):
         try:
             # Issue a NETCONF <copy-config> operation
@@ -86,135 +258,114 @@ class NetconfClient:
         except Exception as e:
             print(f"Failed to copy configuration. Error: {str(e)}")
 
-    def add_interface(self, interface_name, description):
+
+    # ------------<delete-config>--------------
+
+    def delete_loopback(self, loopback_number:int): 
+        """
+        Delete a loopback on a cisco-nx-os-device
+        """
+        LOOPBACK = {'loopback': 'lo{}'.format(loopback_number),
+                    'name': 'Loopback{}'.format(loopback_number)
+                }
+
+        remove_ip_interface = """
+        <config>
+            <System xmlns="http://cisco.com/ns/yang/cisco-nx-os-device">
+                <intf-items>
+                    <lb-items>
+                        <LbRtdIf-list operation="delete">
+                            <id>{0}</id>
+                        </LbRtdIf-list>
+                    </lb-items>
+                </intf-items>
+            </System>
+        </config>
+        """
+
+        data = remove_ip_interface.format(LOOPBACK['loopback'])
+        netconf_response = self.device.edit_config(target='running', config=data)
+
+        return netconf_response
+
+    # --------------Misc--------------
+
+    def _save_dict_to_file(self, dictionary, file_name):
         try:
-            # Prepare the XML data for the <edit-config> operation
-            edit_data = f"""
-                <config xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
-                    <interface-configurations xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-ifmgr-cfg">
-                        <interface-configuration>
-                            <active>act</active>
-                            <interface-name>{interface_name}</name>
-                            <description>{description}</description>
-                        </interface>
-                    </interface-configurations>
-                </config>
-            """
-
-            # Issue a NETCONF <edit-config> operation
-            self.device.edit_config(target='running', config=edit_data)
-
-            print(f"Interface '{interface_name}' added successfully.")
+            with open(file_name, 'w') as file:
+                for value in dictionary:
+                    file.write(value+"\n")
+            print(f"Dictionary saved to {file_name}")
 
         except Exception as e:
-            print(f"Failed to add interface. Error: {str(e)}")
+            print(f"Failed to save dictionary to {file_name}. Error: {str(e)}")
+
+    def _save_str_to_file(self, string, file_name):
+        try:
+            with open(file_name, 'w') as file:
+                file.write(string)
+            print(f"String saved to {file_name}")
+
+        except Exception as e:
+            print(f"Failed to save string to {file_name}. Error: {str(e)}")
 
 
-# Example usage:
 if __name__ == "__main__":
 
-    # # Replace with your device's details
-    # netconf_instance = NetconfClient(
-    #     host="sandbox-iosxr-1.cisco.com",
-    #     port=830,  # Default NETCONF port
-    #     username="admin",
-    #     password="C1sco12345",
-    # )
+    # Sandbox #1
+    netconf_instance = NetconfClient(
+        host="sandbox-iosxr-1.cisco.com",
+        port=830,
+        username="admin",
+        password="C1sco12345",
+    )
 
-    # Replace with your device's details
+    # Sandbox #2
     netconf_instance = NetconfClient(
         host="sbx-nxos-mgmt.cisco.com",
-        port=830,  # Default NETCONF port
+        port=830,
         username="admin",
         password="Admin_1234!"
     )
 
     netconf_instance.connect()
 
-    # Perform NETCONF operations here
+    # ------------<get-config>--------------
 
-    filter_str = f"""
-<filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
-    <interface-configurations xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-ifmgr-cfg"/>
-</filter>
-"""
-    edit_data = f"""
-<config xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
-    <interface-configurations xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-ifmgr-cfg">
-        <interface-configuration>
-            <active>act</active>
-            <interface-name>JACHM</interface-name>
-            <description>test</description>
-        </interface-configuration>
-    </interface-configurations>
-</config>
-"""
-
-    edit_data = f"""
-<config xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
-   <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
-      <interface>
-         <name xmlns:nc="urn:ietf:params:xml:ns:netconf:base:1.0">GigabitEthernet3</name>
-         <description>This is a test</description>
-         <type xmlns:ianaift="urn:ietf:params:xml:ns:yang:iana-if-type">ianaift:ethernetCsmacd</type>
-         <enabled>false</enabled>
-         <ipv4 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">
-            <address>
-               <ip>87.2.3.4</ip>
-               <netmask>255.255.255.0</netmask>
-            </address>
-         </ipv4>
-         <ipv6 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip"/>
-      </interface>
-   </interfaces>
-</config>
-"""
-
-
-    loopback_filter = """
-    <filter>
-        <System xmlns="http://cisco.com/ns/yang/cisco-nx-os-device">
-            <intf-items>
-                <lb-items></lb-items>
-            </intf-items>
-        </System>
-    </filter>"""
-
-    ipv4_filter_template = """
-    <filter>
-        <System xmlns="http://cisco.com/ns/yang/cisco-nx-os-device">
-            <ipv4-items>
-                <inst-items>
-                    <dom-items>
-                        <Dom-list>
-                            <name>default</name>
-                            <if-items>
-                                <If-list>
-                                    <id>{0}</id>
-                                </If-list>
-                            </if-items>
-                        </Dom-list>
-                    </dom-items>
-                </inst-items>
-            </ipv4-items>
-        </System>
-    </filter>"""
-    print(netconf_instance.get_config(save_to_file=True,filter_str=loopback_filter))
-    #print(netconf_instance.get_config(save_to_file=True))
-
+    filter_str = """
     
+    """
+
+    # print(netconf_instance.get_config(save_to_file=True,filter_str=filter_str))
+    # netconf_instance.get_config(save_to_file=True)
+
+    # netconf_instance.get_capabilities(save_to_file=True)
+
+    # print(netconf_instance.get_serial())
+
+    print(netconf_instance.get_loopbacks())
+
+    # schema = 'Cisco-NX-OS-device' 
+    # print(netconf_instance.get_schema(schema,save_to_file=True))
+
+
+    # ------------<edit-config>--------------
+
+    edit_data = """
+    
+    """
+
     # netconf_instance.edit_config(edit_data, target='running')
-    #print(netconf_instance.get_config(save_to_file=True,filter_str=filter_str))
 
+    # print(netconf_instance.add_loopback(11,'192.168.0.1/24','JACHM TEST'))
 
-    # netconf_instance.show_interfaces()
+    # ------------<delete-config>--------------
 
-    for capability in netconf_instance.get_capabilities():
-        print('*'* 50)
-        print(capability)
+    #print(netconf_instance.delete_loopback(11))
 
-    # netconf_instance.add_interface("JACHM","test")
+    # ------------<copy-config>--------------
 
     # netconf_instance.copy_config('running','cadidate')
+
 
     netconf_instance.disconnect()
